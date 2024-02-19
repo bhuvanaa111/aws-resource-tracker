c2090d670a10d685db40e0aeaf435f2670ab8ff9
bhuvanaa111 <bhuvanakarnati89@gmail.com>
2024-02-19 15:00:59 +0000

    intial commit-resource shell script file added

---
+++ aws-reource-tracker.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+########################
+# Author:
+# Date:
+#
+# version:v1
+#
+# This script will report the aws reosurces usage
+# #######################
+
+# aws S3
+# aws EC2
+# aws Lambda
+# aws iam Users
+
+# list s3 buckets
+echo "print list of s3 buckets"
+aws s3 ls > resourceTracker
+
+# list Ec2 instances
+echo "print ec2 instances"
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+
+# list lambda
+echo "print lambda functions"
+aws lambda list-functions >> resourceTracker
+
+# list iam users
+echo "print iam users"
+aws iam list-users
+
